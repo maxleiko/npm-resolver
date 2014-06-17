a5afbf7cd82b0f21d0a16081dc374977c02f21e0
Maxime Tricoire <max.tricoire@gmail.com>
2014-06-17 13:15:55 +0200

    clean

--- src/main/java/org/kevoree/npm/resolver/NpmResolver.java
+++ src/main/java/org/kevoree/npm/resolver/NpmResolver.java
@@ -2,6 +2,8 @@ package org.kevoree.npm.resolver;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 
 import java.io.*;
@@ -23,7 +25,9 @@ public class NpmResolver {
      */
     public File resolve(String pkgName, String pkgVersion) throws Exception {
         if (pkgName != null && pkgName.length() > 0) {
-            if (pkgVersion == null || (pkgVersion != null && pkgVersion.length() == 0)) pkgVersion = "latest";
+            if (pkgVersion == null || (pkgVersion != null && pkgVersion.length() == 0)) {
+                pkgVersion = "latest";
+            }
 
             URL url = new URL("https://registry.npmjs.org/"+pkgName+"/"+pkgVersion);
             URLConnection conn = url.openConnection();
@@ -31,11 +35,12 @@ public class NpmResolver {
             JsonParser parser = new JsonParser();
             JsonObject response = (JsonObject) parser.parse(new InputStreamReader(is));
             is.close();
+            pkgVersion = response.get("version").getAsString();
 
             if (response.has("dist") && response.getAsJsonObject("dist").has("tarball")) {
                 URL tarUrl = new URL(response.getAsJsonObject("dist").get("tarball").getAsString());
                 InputStream in = tarUrl.openStream();
-                File tgzFile = File.createTempFile(pkgName + "-" + pkgVersion, ".tgz");
+                File tgzFile = File.createTempFile(pkgName + "-" + pkgVersion + "_", ".tgz");
                 FileOutputStream fos = new FileOutputStream(tgzFile);
                 byte data[] = new byte[1024];
                 int count;
@@ -47,16 +52,17 @@ public class NpmResolver {
 
                 FileInputStream fin = new FileInputStream(tgzFile.getAbsolutePath());
                 BufferedInputStream bis = new BufferedInputStream(fin);
-                File tarFile = File.createTempFile(pkgName + "-" + pkgVersion, ".tar");
+                File tarFile = File.createTempFile(pkgName + "-" + pkgVersion + "_", ".tar");
                 FileOutputStream out = new FileOutputStream(tarFile.getAbsolutePath());
                 GzipCompressorInputStream gzIn = new GzipCompressorInputStream(bis);
                 final byte[] buffer = new byte[1024];
-                int n = 0;
+                int n;
                 while (-1 != (n = gzIn.read(buffer))) {
                     out.write(buffer, 0, n);
                 }
                 out.close();
                 gzIn.close();
+                tgzFile.delete();
 
                 return tarFile;
 
@@ -65,6 +71,35 @@ public class NpmResolver {
             }
         } else {
             throw new Exception("You must give a package name to resolve");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        // TODO: refactor as a proper test
+        NpmResolver resolver = new NpmResolver();
+        File resolved = resolver.resolve("kevoree-chan-websocket", "latest");
+        if (resolved != null && resolved.exists()) {
+            TarArchiveInputStream tarInput = new TarArchiveInputStream(new FileInputStream(resolved));
+            try {
+                TarArchiveEntry entry;
+                while (null!=(entry=tarInput.getNextTarEntry())) {
+                    if (entry.getName().equals("package/kevlib.json")) {
+                        int entrySize = (int) entry.getSize();
+                        byte[] content = new byte[entrySize];
+                        tarInput.read(content, 0, entrySize);
+                        File output = File.createTempFile("kevoree-chan-websocket_latest", ".json");
+                        FileOutputStream fos = new FileOutputStream(output);
+                        fos.write(content);
+                        fos.close();
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                tarInput.close();
+            }
+        } else {
+            System.out.println("NpmResolver error: Can't resolve TypeDefinition kevoree-chan-websocket:latest");
         }
     }
 }
